7310ee13fa0236bc2978ce662c724d75bfa2bb12
Kris Dekeyser <kris.dekeyser@libis.be>
2022-07-25 18:02:20 +0200

    tasks

---
+++ lib/teneo/data_model/task.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Teneo::DataModel
+  class Task < Teneo::DataModel::Base
+    include WithParameters
+
+    STAGE_LIST = Teneo::DataModel::IngestStage::STAGE_LIST
+
+    def validate
+      validates_presence :stage, :name, :class_name
+      validates_includes STAGE_LIST, :stage
+    end
+  end
+end
