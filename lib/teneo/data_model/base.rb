d4ddcee0c998638920965e07366c98f31a55223d
Kris Dekeyser <kris.dekeyser@libis.be>
2022-07-25 16:31:22 +0200

    Base model fixes

--- lib/teneo/data_model/base.rb
+++ lib/teneo/data_model/base.rb
@@ -4,9 +4,21 @@ module Teneo::DataModel
   Base = Class.new(Sequel::Model(Teneo::DataModel::Database.connect))
   Base.require_valid_table = false
 
-  Base.def_Model( Teneo::DataModel )
+  Base.def_Model(Teneo::DataModel)
 
   class Base
-  end
+    def to_hash
+      super().reject { |k, v| volatile_attributes.include?(k) }
+    end
 
+    protected
+
+    def volatile_attributes
+      %i'id created_at updated_at lock_version'
+    end
+
+    def copy_attributes(other)
+      self.set(other.to_hash).each_with_object({}) { |(k, v), h| h[k] = v.duplicatable? ? v.dup : v }
+    end
+  end
 end
